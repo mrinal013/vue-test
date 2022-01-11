dd0e412956764ca382c476abd085c207b6418b3d
Mrinal Haque <mrinalhaque99@gmail.com>
2022-01-11 12:36:38 +0000

    basic structure add

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Events from '../views/Events.vue'
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
 })
 
